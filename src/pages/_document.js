7b395a12d39b2ba2ab2558d6f0bf624b891ea642
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-31 15:28:38 -0300

    link para fontes do google

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="author" content="Melissa" />
-      </Head>
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet" />
+        </Head>
       <body>
         <Main />
         <NextScript />
