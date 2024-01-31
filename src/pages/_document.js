51fc6a522680bc3c04c14a1a98f607a2352f9a4e
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-31 11:55:13 -0300

    iniciando o projeto

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="author" content="Melissa" />
+      </Head>
       <body>
         <Main />
         <NextScript />
