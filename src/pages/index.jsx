3db916ef9c18f13441d09f87e31b6e7baa6955e3
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-31 15:29:11 -0300

    teste de estilização

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,33 @@
 import Menu from "@/components/Menu";
 import Head from "next/head";
+import styled from "styled-components";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>STEPHEN KING - 2024</title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-      </Head>
-      <Menu></Menu>
+      <StyledHome>
+        <Head>
+          <title>STEPHEN KING - 2024</title>
+          <meta name="description" content="" />
+          <meta name="keywords" content="" />
+        </Head>
+        <Menu></Menu>
 
-      <h1> Desafio Next</h1>
-      <h3>Livros</h3>
+        <h1> STEPHEN KING</h1>
+        <h3>Livros</h3>
+      </StyledHome>
     </>
   );
 }
+
+const StyledHome = styled.section`
+  background-color: #001020;
+
+  h1 {
+    font-family: "Creepster", system-ui;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 5rem;
+    color: #eae5d3;
+  }
+`;
