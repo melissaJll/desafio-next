b62eebd5ae72c62ac59d27d43c44a167d6f86d5e
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-31 11:55:27 -0300

    index

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Menu from "@/components/Menu";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>STEPHEN KING - 2024</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+      </Head>
+      <Menu></Menu>
+
+      <h1> Desafio Next</h1>
+      <h3>Livros</h3>
+    </>
+  );
+}
