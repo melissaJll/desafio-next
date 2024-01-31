4ea3e0f931833a333d06a1266160d4ccfb4c9834
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-31 16:03:36 -0300

    conteúdo simples nas páginas

---
+++ src/pages/contos.jsx
@@ -0,0 +1,9 @@
+export default function Contos() {
+  return (
+    <>
+      <section>
+        <h2>Contos</h2>
+      </section>
+    </>
+  );
+}
---
+++ src/pages/livros.jsx
@@ -0,0 +1,9 @@
+export default function Livros() {
+  return (
+    <>
+      <section>
+        <h2>Livros</h2>
+      </section>
+    </>
+  );
+}
