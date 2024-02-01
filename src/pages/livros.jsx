db75231e20982894b6850620593ea33861dc64ec
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-01 11:27:34 -0300

    static paratrazer os livros

--- src/pages/livros.jsx
+++ src/pages/livros.jsx
@@ -1,9 +1,36 @@
-export default function Livros() {
+import ListaLivros from "@/components/ListaLivros";
+import serverApi from "./api/server";
+
+export async function getStaticProps() {
+  try {
+    const resposta = await fetch(`${serverApi}books`);
+    const dados = await resposta.json();
+    // console.log(dados);
+
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+
+    return {
+      props: {
+        livros: dados.data,
+      },
+    };
+  } catch (error) {
+    console.error("Deu ruim: " + error.message);
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default function Livros({ livros }) {
   return (
     <>
       <section>
         <h2>Livros</h2>
       </section>
+      {/* <ListaLivros livros={livros} /> */}
     </>
   );
 }
