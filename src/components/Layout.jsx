8dc02254c7c146a74e90642b2268cca40354688f
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-01 11:17:58 -0300

    Animação do Título

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,8 +5,8 @@ export default function Layout({ children }) {
   return (
     <>
       <StyledLayout>
+        <h1 className="blink_me"> STEPHEN KING</h1>
         <Menu></Menu>
-        <h1> STEPHEN KING</h1>
         <main>{children}</main>
       </StyledLayout>
     </>
@@ -14,11 +14,34 @@ export default function Layout({ children }) {
 }
 
 const StyledLayout = styled.div`
+  background-color: #001020;
+
+  .blink_me {
+    animation: blinker 14s ease-out infinite;
+  }
+
+  @keyframes blinker {
+    50% {
+      opacity: 0;
+      color: #ff0000;
+    }
+  }
+
   h1 {
     font-family: "Creepster", system-ui;
     font-weight: 400;
     font-style: normal;
-    font-size: 5rem;
+    font-size: 7rem;
     color: #eae5d3;
+    text-align: center;
+    padding: 4rem;
+    &:hover {
+      color: #ec1111;
+    }
+  }
+  main {
+    background-color: #fffdf0;
+    border-radius: 3px;
+    padding: 2rem;
   }
 `;
