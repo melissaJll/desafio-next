83d82b895b7e29187e284afb39387d46be02bcb8
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-01 19:46:17 -0300

    api

---
+++ src/pages/api/server.js
@@ -0,0 +1,7 @@
+// const porta = 2112;
+const endereco = "stephen-king-api.onrender.com/api";
+// 10.20.47.34
+
+const serverApi = `http://${endereco}`;
+
+export default serverApi;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,35 @@
 import Menu from "@/components/Menu";
 import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
 import styled from "styled-components";
+import serverApi from "./api/server";
 
-export default function Home() {
+export async function getStaticProps() {
+  try {
+    const resposta = await fetch(`${serverApi}/books`);
+    const dados = await resposta.json();
+    // console.log(dados);
+
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+    return {
+      props: {
+        posts: dados.data,
+      },
+    };
+  } catch (error) {
+    console.error("Deu ruim: " + error.message);
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default function Home({ posts }) {
+  const [listaDePosts, setListaDePosts] = useState(posts);
+
   return (
     <>
       <Head>
@@ -14,6 +41,16 @@ export default function Home() {
       <StyledHome>
         <h1> STEPHEN KING</h1>
         <h3>Livros</h3>
+        {posts.map((post) => {
+          return (
+            <article key={post.id}>
+              <Link href={`/books/${post.id}`}>
+                <h3>{post.Title}</h3>
+                <p>{post.Pages} </p>
+              </Link>
+            </article>
+          );
+        })}
       </StyledHome>
     </>
   );
