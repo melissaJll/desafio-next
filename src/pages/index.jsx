934b651472e0c632b6ef9375e5bad23d94a1d822
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-02 11:32:05 -0300

    Estrutura da página principal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,13 +37,29 @@ export default function Home({ livros }) {
       </Head>
 
       <StyledHome>
-        <h3>Home</h3>
+        <p className="saudacao">
+          <span>Bem-vindo</span> ao Universo de Stephen King! Explore o mundo
+          sombrio e intrigante criado por Stephen King, onde o
+          <span> terror</span> se mistura ao suspense em <span>histórias</span>{" "}
+          envolventes que desafiam a imaginação. Prepare-se para mergulhar em
+          tramas complexas e personagens icônicos que prendem sua atenção do
+          início ao <span>Fim</span>.
+        </p>
 
+        <h1>O Autor</h1>
+        {/* 
         <Image
           className="circle-image"
           width={320}
           height={250}
           src="/images/stephen-king.jpg"
+          alt="autor stephen king"
+        /> */}
+        <Image
+          className="circle-image"
+          width={340}
+          height={320}
+          src="/images/stephen-king-gq.webp"
           alt="autor stephen king"
         />
         <p>
@@ -61,25 +77,69 @@ export default function Home({ livros }) {
           found work in the kitchens of Pineland, a nearby residential facility
           for the mentally challenged.
         </p>
+        <p>
+          Stephen attended the grammar school in Durham and then Lisbon Falls
+          High School, graduating in 1966. From his sophomore year at the
+          University of Maine at Orono, he wrote a weekly column for the school
+          newspaper, THE MAINE CAMPUS. He was also active in student politics,
+          serving as a member of the Student Senate. He came to support the
+          anti-war movement on the Orono campus, arriving at his stance from a
+          conservative view that the war in Vietnam was unconstitutional. He
+          graduated from the University of Maine at Orono in 1970, with a B.A.
+          in English and qualified to teach on the high school level. A draft
+          board examination immediately post-graduation found him 4-F on grounds
+          of high blood pressure, limited vision, flat feet, and punctured
+          eardrums.
+        </p>
+        <p>
+          He and Tabitha Spruce married in January of 1971. He met Tabitha in
+          the stacks of the Fogler Library at the University of Maine at Orono,
+          where they both worked as students. As Stephen was unable to find
+          placement as a teacher immediately, the Kings lived on his earnings as
+          a laborer at an industrial laundry, and her student loan and savings,
+          with an occasional boost from a short story sale to men's magazines.
+        </p>
       </StyledHome>
     </>
   );
 }
 
 const StyledHome = styled.section`
-  max-width: 700px;
+  max-width: 1000px;
+  margin: auto;
   h1 {
+    color: #b1936b;
+  }
+  p {
+    padding-right: 1rem;
+    color: #997039;
+    letter-spacing: 1px;
+    line-height: 23px;
+  }
+  span {
+    font-weight: bold;
+    font-size: 20px;
+    color: #a31e07;
+  }
+  .saudacao {
+    margin: 5rem auto;
+    text-align: center;
+    letter-spacing: 4px;
+    line-height: 24px;
   }
   .circle-image {
     border-radius: 70%; /* faz com que o contêiner tenha a forma de um círculo */
     overflow: hidden; /* garante que a imagem não ultrapasse os limites do círculo */
-    display: flex; /* usa flexbox para alinhamento */
-    justify-content: center; /* centraliza horizontalmente */
-    align-items: center; /* centraliza verticalmente */
+    /* display: flex; usa flexbox para alinhamento */
+    /* justify-content: center; centraliza horizontalmente */
+    /* align-items: center; centraliza verticalmente */
+    float: right;
   }
 
   .circle-image img {
     width: auto; /* mantém a proporção da imagem */
     height: auto; /* mantém a proporção da imagem */
+    margin: 1rem;
+    margin-bottom: 10rem;
   }
 `;
