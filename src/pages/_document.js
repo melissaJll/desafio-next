f475546c501a544eaca0f91d2fdde67547c08759
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-04 20:41:39 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,12 @@ export default function Document() {
         <meta name="apple-mobile-web-app-title" content="PetShop" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#000000" />
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.png"
+          type="image/png"
+          sizes="256x256"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
