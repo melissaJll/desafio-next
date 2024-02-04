1f64c6c54f3f039cb6c98b61cffef2f51134835b
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-04 20:55:49 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
-
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: { styledComponents: true },
 };
-module.exports = withPWA(nextConfig);
+
+export default nextConfig;
