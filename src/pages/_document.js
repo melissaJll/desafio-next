1e7427fd5f70a379c2e1289f28b4e0e60f5cc807
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-04 19:08:48 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,20 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="author" content="Melissa" />
-        
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="application-name" content="PetShop" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PetShop" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet" />
-        </Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Creepster&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
